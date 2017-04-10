9d3f09b330d1e09c701fe7803478480170738272
rocket1 <jazzmongrel@gmail.com>
2017-04-10 10:06:26 -0700

    Uncomment out scroll code and focus code.

--- js/main.js
+++ js/main.js
@@ -1,22 +1,22 @@
-//var distance = $("#contactForm").position().top;
-//var windowHeight = $(window).height();
-//var formDistance = distance - windowHeight;
-//
-//$(document).ready(function(){
-//    $(window).scroll(function(){
-//        if ($(window).scrollTop() > formDistance){
-//            console.log("entered!"+formDistance);
-//            focusInput($("#Name"));
-//        }
-//        else if($(window).scrollTop() < formDistance){
-//            console.log("exited!"+formDistance);
-//            $("#Name").blur();
-//        }
-//    });
-//});
-//
-//function focusInput(elem) {
-//  var x = window.scrollX, y = window.scrollY;
-//  elem.focus();
-//  window.scrollTo(x, y);
-//}
+var distance = $("#contactForm").position().top;
+var windowHeight = $(window).height();
+var formDistance = distance - windowHeight;
+
+$(document).ready(function(){
+    $(window).scroll(function(){
+        if ($(window).scrollTop() > formDistance){
+            console.log("entered!"+formDistance);
+            focusInput($("#Name"));
+        }
+        else if($(window).scrollTop() < formDistance){
+            console.log("exited!"+formDistance);
+            $("#Name").blur();
+        }
+    });
+});
+
+function focusInput(elem) {
+  var x = window.scrollX, y = window.scrollY;
+  elem.focus();
+  window.scrollTo(x, y);
+}
