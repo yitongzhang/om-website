ef8a4f7a733a4ea80a30fb8591306ec6c35aa117
rocket1 <jazzmongrel@gmail.com>
2017-04-11 09:40:04 -0700

    Nav now reflects current page.

--- nav.php
+++ nav.php
@@ -1,4 +1,14 @@
+<?php
+
+$url = $_SERVER['REQUEST_URI'];
+$isStudio = preg_match('/studio/', $url);
+$isContact = preg_match('/contact/', $url);
+
+?>
+
 <nav>
-    <div class="logo"><span class="selected">O/M</span></div>
-    <div class="subnav"><a href="studio.php">Studio</a>  &nbsp; &nbsp; &nbsp; <a href="contact.php">Contact</a></div>
+    <div class="logo"><span>O/M</span></div>
+    <div class="subnav">
+        <a href="studio.php" class="<?php $isStudio && print 'selected'; ?>">Studio</a> &nbsp; &nbsp; &nbsp;
+        <a href="contact.php" class="<?php $isContact && print 'selected'; ?>">Contact</a></div>
 </nav>
