3828b49b3c746f28e9a2f238d12f1b2b87c374cf
rocket1 <jazzmongrel@gmail.com>
2017-04-18 09:54:45 -0700

    Major refactor of CSS.

--- index.php
+++ index.php
@@ -1,18 +1,8 @@
 <!DOCTYPE html>
 <html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-	<meta http-equiv="Content-Type" content="text/html" charset=utf-8" name="viewport" content="width=device-width, initial-scale=1.0" >
-	<title>O/M: A product design studio in San Francisco</title>
-	<link rel="icon" type="image/png" href="img/favicon.png">
-	<meta name="description" content="O/M: A product design studio in San Francisco" />
-	<meta name="keywords"  content="O/M, product design, user experience design, user interface design, ui design, design, mobile design, software, software development, website design, app design" />
-	<meta name="Resource-type" content="Document" />
-	<link rel="stylesheet" type="text/css" href="css/reset.css" />
-	<link rel="stylesheet" type="text/css" href="css/MyFontsWebfontsKit.css" />
-	<link rel="stylesheet" type="text/css" href="css/default.css" />
-	<link rel="stylesheet" type="text/css" href="css/default-mobile-yz.css" />
-	<link rel="stylesheet" type="text/css" href="css/type.css" />
-</head>
+
+<?php include 'head.php'; ?>
+
 <body>
 
 <div class="top-bar"></div>
@@ -105,17 +95,7 @@
     </div>
 </footer>
 
-<script>
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+<?php include 'foot.php'; ?>
 
-  ga('create', 'UA-52539801-1', 'auto');
-  ga('send', 'pageview');
-</script>
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
-<script src="js/main.js"></script>
-<script src="js/contact.js"></script>
 </body>
 </html>
